bf1eea96e2f1029ff112ede0965431a1c605a907
Nozomu Hatta <nozomuhatta@gmail.com>
2020-07-04 02:34:54 +0900

    Add thread func

--- main.cpp
+++ main.cpp
@@ -4,14 +4,20 @@
 #include <sys/eventfd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <thread>
 
 class Worker {
 private:
   int sock_, eventfd_;
+  std::thread th_;
+  void ThreadFunc();
 
 public:
   Worker(const char *ip, const int port);
+  void Start();
 };
+
+void Worker::ThreadFunc() {}
 
 Worker::Worker(const char *ip, const int port) {
   sock_ = socket(AF_INET, SOCK_DGRAM, 0);
@@ -23,6 +29,10 @@ Worker::Worker(const char *ip, const int port) {
   bind(sock_, (struct sockaddr *)&addr, sizeof(addr));
 
   eventfd_ = eventfd(0, 0);
+}
+
+void Worker::Start() {
+  th_ = std::thread([this] { ThreadFunc(); });
 }
 
 int main(int argc, char const *argv[]) {
